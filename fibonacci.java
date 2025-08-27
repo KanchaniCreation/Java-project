07e80e43a4baaea20d7cf4445fa4b84a40806078
KanchaniCreation <hkanchanigupta@gmail.com>
2025-08-27 21:35:29 +0530

    update

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+public class fibonacci {
+    public static void main(String[] args){
+        int n = 10;
+        int first = 0,second=1;
+        System.out.println(("fibonacci series up to"));
+        for( int i=1;i<=n;i++){
+            System.out.println(first+"");
+            int next = first+second;
+            first=second;
+            second=next;
+        }
+    }
+    
+}

ca3ddcf0f564bc80d0f7de5d505d5c42e5c10093
KanchaniCreation <hkanchanigupta@gmail.com>
2025-08-27 09:51:50 +0530

    Added new Java file

---
+++ swap.java
@@ -0,0 +1,14 @@
+public class swap {
+    public static void main(String[]args){
+        int a = 5, b=2 , c;
+        c=a; //c=5
+        a=b; //a=2
+        b=c; //b=5
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+    
+}

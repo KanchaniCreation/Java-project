7bba118fbe188da4f2b1794a11481470df1b9b24
KanchaniCreation <hkanchanigupta@gmail.com>
2025-08-27 20:23:04 +0530

    update

--- swap.java
+++ swap.java
@@ -1,14 +1,15 @@
 public class swap {
     public static void main(String[]args){
-        int a = 5, b=2 , c;
-        c=a; //c=5
-        a=b; //a=2
-        b=c; //b=5
+        int a = 5, b=2  ;
+       // c=a; //c=5
+      //  a=b; //a=2
+      //  b=c; //b=5
+      //without using third variable
+        a=a+b;
+        a=a-b;
+        a=a-b;
         System.out.println(a);
         System.out.println(b);
 
-
-
     }
-    
 }

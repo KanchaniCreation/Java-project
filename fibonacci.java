1501aa2c262cf90e5157355e04726b60fbd9f619
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-03 20:54:29 +0530

    update

--- fibonacci.java
+++ fibonacci.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 public class fibonacci {
     public static void main(String[] args){
-        int n = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number where u get the series ");
+        int num = sc.nextInt();
         int first = 0,second=1;
         System.out.println(("fibonacci series up to"));
-        for( int i=1;i<=n;i++){
+        for( int i=1;i<=num;i++){
             System.out.println(first+"");
             int next = first+second;
             first=second;

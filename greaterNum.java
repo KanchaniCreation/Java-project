e3002075fcb6000280192d1b7de5d3bce5542d70
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-04 21:17:59 +0530

    update

---
+++ greaterNum.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner ;
+public class greaterNum {
+	public static void main(String[] args) {
+		//Scanner sc = new Scanner(System.in);
+	int a =4,b=1,c=5;
+	if (a>b) {
+		if(a>c) {
+		System.out.println("a is greatest");
+	}
+	}
+	else {
+		System.out.println("a is smallest");
+	}
+    
+	}
+}
+
+

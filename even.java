87a448c8ad9b29623f2f36b6920c280bebdb4faf
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-05 17:56:35 +0530

    update

---
+++ even.java
@@ -0,0 +1,12 @@
+public class even {
+public static void main(String[] args){
+    int a=8;
+    if (a%2==0){
+        System.out.print("a is prime number ");
+    }
+    else {
+        System.out.print("a is odd");
+    }
+}    
+    
+}

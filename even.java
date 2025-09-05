8eac9c7e888222bf01ec7a3811c6e79daeed6577
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-05 18:11:48 +0530

    update

--- even.java
+++ even.java
@@ -1,6 +1,9 @@
+import java.util.Scanner ;
 public class even {
 public static void main(String[] args){
-    int a=8;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number ");
+    int a = sc.nextInt();
     if (a%2==0){
         System.out.print("a is prime number ");
     }

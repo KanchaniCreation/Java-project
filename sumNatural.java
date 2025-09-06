35a2bfd0d2e37c216785573d3aa9ced674cc7a21
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-06 22:30:48 +0530

    update

--- sumNatural.java
+++ sumNatural.java
@@ -1,9 +1,13 @@
+import java.util.Scanner ;
 public class sumNatural {
      
 public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number ");
+    int num = sc.nextInt();
 
     int sum=0,val=1;
-    while(val<=10) {
+    while(val<=num) {
     	sum=  sum +val;
     	System.out.println(sum);
     	val++ ;

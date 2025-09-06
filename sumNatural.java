37e8dfe3e1bda6c616fb01fdd11273017a8e5549
KanchaniCreation <hkanchanigupta@gmail.com>
2025-09-06 22:22:47 +0530

    update

---
+++ sumNatural.java
@@ -0,0 +1,19 @@
+public class sumNatural {
+     
+public static void main(String[] args) {
+
+    int sum=0,val=1;
+    while(val<=10) {
+    	sum=  sum +val;
+    	System.out.println(sum);
+    	val++ ;
+   
+
+        }
+   System.out.println(sum);
+
+
+
+    }
+    
+    }
